36c96b983252a9cc757690cb88f69e22fe3f6d0b
patriceammah <patriceammah@gmail.com>
2026-01-28 13:35:16 -0500

    test file updated

--- application/src/test/java/org/thingsboard/server/service/tracking/FunctionCallCounterServiceTest.java
+++ application/src/test/java/org/thingsboard/server/service/tracking/FunctionCallCounterServiceTest.java
@@ -1,18 +1,15 @@
 package org.thingsboard.server.service.tracking;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class FunctionCallCounterServiceTest {
